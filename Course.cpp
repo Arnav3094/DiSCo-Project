06cdb0db0fa408cee7964bd51f8cf875608e4753
arnav3094pvt@gmail.com <arnav3094pvt@gmail.com>
2023-11-27 14:36:32 +0530

    Add vector for allotted professors in Course class and add vector for allotted courses in Professor class

--- Course.cpp
+++ Course.cpp
@@ -5,13 +5,16 @@ class Course{
     string name;
     int courseCode;
     int type;
-    vector <Professor&> professors;
     /**
      * 1 is FDCDC
      * 2 is FDEL
      * 3 is HDCDC
      * 4 is HDEL
     */
+    vector <Professor&> professors;
+    vector <Professor&> allottedTo; 
+    // We wanted to keep a cap on the number of professors that can be allotted to a course
+    // but could not find a way to keep a cap on the maximum size of the vector
     public:
         Course(string name, int courseCode, int type){
             this->name = name;
@@ -36,4 +39,20 @@ class Course{
         void setProfessors(vector<Professor&>& professors){
             this->professors = professors;
         }
+        vector<Professor&>& getAllottedTo(){
+            return allottedTo;
+        }
+        void setAllottedTo(vector<Professor&>& allottedTo){
+            if (allottedTo.size() > 2){
+                throw invalid_argument("ERROR: Cannot allot more than 2 professors to a course.\nPlease allot only 2 professors to this course.");
+            }
+            this->allottedTo = allottedTo;
+        }
+        void allotTo(Professor& professor){
+            if(allottedTo.size() >= 2){
+                throw invalid_argument("ERROR: Cannot allot more than 2 professors to a course.\nPlease allot only 2 professors to this course.");
+            }
+            allottedTo.push_back(professor);
+        }
+
 };
--- Professor.cpp
+++ Professor.cpp
@@ -4,37 +4,57 @@ using namespace std;
 class Professor{
     string name;
     int facultyCode;
-    vector <int> preference;
-    vector <Course&> courses;
+    int category;
+    /**
+     * 1 is x1
+     * 2 is x2
+     * 3 is x3 
+    */
+    vector <Course&> courses; // This is the preference list of the prof
+    vector <Course&> allotted;
     public:
-        Professor(string name, int facultyCode, vector<int>& preference){
+        Professor(string name, int facultyCode, int category, vector<Course&>& courses){
             this->name = name;
             this->facultyCode = facultyCode;
-            this->preference = preference;
+            this->category = category;
+            this->courses = courses;
         }
-        Professor(string name, int facultyCode){
+        Professor(string name, int facultyCode, int category){
             this->name = name;
             this->facultyCode = facultyCode;
-        }
-        vector<int>& getPrefs(){
-            return preference;
-        }
-        void setPrefs(vector<int>& preference){
-            this->preference = preference;
-        }
-        int getFacultyCode(){
-            return facultyCode;
+            this->category = category;
         }
         string getName(){
             return name;
         }
-        void addCourse(Course& course){
-            courses.push_back(course);
+        int getFacultyCode(){
+            return facultyCode;
+        }
+        int getCategory(){
+            return category;
         }
         void setCourses(vector<Course&>& courses){
             this->courses = courses;
         }
+        void addCourse(Course& course){
+            courses.push_back(course);
+        }
         vector<Course&>& getCourse(){
             return courses;
+        }
+        void setAllotted(vector<Course&>& allotted){
+            if(allotted.size() > category){
+                throw invalid_argument("ERROR: Cannot allot more than " + to_string(category) + " courses to a professor.\nPlease allot only " + to_string(category) + " courses to this professor.");
+            }
+            this->allotted = allotted;
+        }
+        void allotCourse(Course& course){
+            if(allotted.size() >= category){
+                throw invalid_argument("ERROR: Cannot allot more than " + to_string(category) + " courses to a professor.\nPlease allot only " + to_string(category) + " courses to this professor.");
+            }
+            allotted.push_back(course);
+        }
+        vector<Course&>& getAllotted(){
+            return allotted;
         }
 };
